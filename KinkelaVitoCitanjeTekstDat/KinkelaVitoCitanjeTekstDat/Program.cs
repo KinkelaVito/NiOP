e2e4b9d37d035bdc09c17d2ae18cc82f29d072b9
KinkelaVito <44948259+KinkelaVito@users.noreply.github.com>
2019-01-16 17:26:55 +0100

    Add files via upload

---
+++ KinkelaVitoCitanjeTekstDat/KinkelaVitoCitanjeTekstDat/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinkelaVitoCitanjeTekstDat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\Učenik\Desktop\dule.txt");
+
+            //čita datoteku liniju po liniju
+            while (!sr.EndOfStream)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+
+            //zatvara
+            sr.Close();
+
+
+            Console.ReadKey();
+        }
+    }
+}
