a9a97e1a39b3c0a1299bd325bb1f57e8587d970b
KinkelaVito <44948259+KinkelaVito@users.noreply.github.com>
2019-01-21 18:43:47 +0100

    Add files via upload

---
+++ KinkelaVitoPictureBox/KinkelaVitoPictureBox/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace KinkelaVitoPictureBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btm1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::KinkelaVitoPictureBox.Properties.Resources._08b24b2389c24ff14ea66df016db5e664a791d01;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(768, 432);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btm1
+            // 
+            this.btm1.Location = new System.Drawing.Point(264, 503);
+            this.btm1.Name = "btm1";
+            this.btm1.Size = new System.Drawing.Size(250, 73);
+            this.btm1.TabIndex = 1;
+            this.btm1.Text = "Prikaži / sakrij sliku";
+            this.btm1.UseVisualStyleBackColor = true;
+            this.btm1.Click += new System.EventHandler(this.btm1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(273, 616);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 659);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btm1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btm1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ KinkelaVitoPictureBox/KinkelaVitoPictureBox/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KinkelaVitoPictureBox
+{
+    public partial class Form1 : Form
+    {
+        bool a = false;
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Hide();
+            textBox1.Hide();
+            
+        }
+
+        private void btm1_Click(object sender, EventArgs e)
+        {
+
+            
+            if (!a)
+            {
+                pictureBox1.Show();              
+            }
+            else
+            {
+                pictureBox1.Hide();
+            }
+            a = !a;
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
