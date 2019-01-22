708bbd7dd834b9f244bcd3d8d8cba769d2060698
KinkelaVito <44948259+KinkelaVito@users.noreply.github.com>
2019-01-22 14:27:28 +0100

    Add files via upload

---
+++ KinkelaVitoImageList/KinkelaVitoImageList/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KinkelaVitoImageList
+{
+    public partial class Form1 : Form
+    {
+        bool a = true;
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = imageList1.Images[0];
+        }
+
+        private void btm1_Click(object sender, EventArgs e)
+        {
+            if (a)
+            {
+                pictureBox1.Image = imageList1.Images[0];
+            }
+            else
+            {
+                pictureBox1.Image = imageList1.Images[1];
+            }
+
+            a = !a;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void oProgramuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 licenca = new Form2();
+            licenca.Show();
+        }
+    }
+}
---
+++ KinkelaVitoImageList/KinkelaVitoImageList/Form2.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace KinkelaVitoImageList
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btmClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btmClose
+            // 
+            this.btmClose.Location = new System.Drawing.Point(12, 78);
+            this.btmClose.Name = "btmClose";
+            this.btmClose.Size = new System.Drawing.Size(75, 23);
+            this.btmClose.TabIndex = 0;
+            this.btmClose.Text = "Zatvori";
+            this.btmClose.UseVisualStyleBackColor = true;
+            this.btmClose.Click += new System.EventHandler(this.btmClose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Vito Kinkela\r\n3.d\r\n22.1.2019\r\n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(120, 113);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btmClose);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btmClose;
+        private System.Windows.Forms.Label label1;
+    }
+}
