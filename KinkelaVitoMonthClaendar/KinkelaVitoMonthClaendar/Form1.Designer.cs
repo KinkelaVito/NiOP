d7ab89ce8ae7b5d027dd44d3220f972a278275bd
KinkelaVito <44948259+KinkelaVito@users.noreply.github.com>
2019-01-29 11:41:55 +0100

    Add files via upload

---
+++ KinkelaVitoMonthClaendar/KinkelaVitoMonthClaendar/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace KinkelaVitoMonthClaendar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mCal1 = new System.Windows.Forms.MonthCalendar();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.oProgramuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btm1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mCal1
+            // 
+            this.mCal1.Location = new System.Drawing.Point(18, 33);
+            this.mCal1.MaxSelectionCount = 99;
+            this.mCal1.Name = "mCal1";
+            this.mCal1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oProgramuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(233, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // oProgramuToolStripMenuItem
+            // 
+            this.oProgramuToolStripMenuItem.Name = "oProgramuToolStripMenuItem";
+            this.oProgramuToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.oProgramuToolStripMenuItem.Text = "O programu";
+            this.oProgramuToolStripMenuItem.Click += new System.EventHandler(this.oProgramuToolStripMenuItem_Click);
+            // 
+            // btm1
+            // 
+            this.btm1.Location = new System.Drawing.Point(18, 207);
+            this.btm1.Name = "btm1";
+            this.btm1.Size = new System.Drawing.Size(199, 80);
+            this.btm1.TabIndex = 2;
+            this.btm1.Text = "Odaberite termin";
+            this.btm1.UseVisualStyleBackColor = true;
+            this.btm1.Click += new System.EventHandler(this.btm1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(233, 309);
+            this.Controls.Add(this.btm1);
+            this.Controls.Add(this.mCal1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar mCal1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem oProgramuToolStripMenuItem;
+        private System.Windows.Forms.Button btm1;
+    }
+}
+
---
+++ KinkelaVitoMonthClaendar/KinkelaVitoMonthClaendar/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KinkelaVitoMonthClaendar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btm1_Click(object sender, EventArgs e)
+        {
+            //objektu klase TimeSpan se dodjeljuju vrijednosti s kalandara
+            TimeSpan ts = mCal1.SelectionEnd - mCal1.SelectionStart;
+
+            //ako je broj odabranih dana manji od 1
+            if (ts.Days < 1)
+            {
+                MessageBox.Show("Uporabom tipke SHIFT odaberite više dana.");
+                return;
+            }
+
+            //ispisuje koliko je dana odabrano
+            MessageBox.Show("Izabrali ste " + ts.Days.ToString() + " dana.", "Rezervacija");
+
+            //ispisuje izabrani interval
+            MessageBox.Show("Vaša rezervacija počinje " + mCal1.SelectionStart.ToShortDateString() + ", a završava " + mCal1.SelectionEnd.ToShortDateString());
+        }
+
+        private void oProgramuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 forma2 = new Form2();
+            forma2.Show();
+        }
+    }
+}
