78210e05ffdd39b21b372c8ba53da4a034a1199e
KinkelaVito <44948259+KinkelaVito@users.noreply.github.com>
2019-02-06 12:03:00 +0100

    Add files via upload

---
+++ KinkelaVito06_02_2019/KinkelaVito06_02_2019/Klase/Vozilo.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinkelaVito06_02_2019.Klase
+{
+    class Vozilo
+    {
+        int brojKotaca;
+        bool vozi, leti, plovi;
+        string vrsta, oznaka;
+
+        public void setBrojKotaca(int brojKotacaPublic)
+        {
+            this.brojKotaca = brojKotacaPublic;
+        }
+        public int getBrojKotaa()
+        {
+            return this.brojKotaca;
+        }
+
+        public void setLeti(bool letiP)
+        {
+            this.leti = letiP;
+        }
+        public bool getLeti()
+        {
+            return this.leti;
+        }
+
+        public void setVozi(bool voziP)
+        {
+            this.vozi = voziP;
+        }
+        public bool getVozi()
+        {
+            return this.vozi;
+        }
+
+        public void setPlovi(bool PloviP)
+        {
+            this.plovi = PloviP;
+        }
+        public bool getPlovi()
+        {
+            return this.plovi;
+        }
+
+        public void setVrsta(string vrstaP)
+        {
+            this.vrsta = vrstaP;
+        }
+        public string getVrsta()
+        {
+            return this.vrsta;
+        }
+
+        public void setOznaka(string oznakaP)
+        {
+            this.oznaka = oznakaP;
+        }
+        public string getOznaka()
+        {
+            return this.oznaka;
+        }
+
+        public Vozilo(int brojKotaca, bool leti, bool plovi, bool vozi, string oznaka, string vrsta)
+        {
+            this.brojKotaca = brojKotaca;
+            this.leti = leti;
+            this.vozi = vozi;
+            this.plovi = plovi;
+            this.vrsta = vrsta;
+            this.oznaka = oznaka;
+        }
+
+        public Vozilo()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "Vrsta: " + vrsta + "\nOznaka: " + oznaka + "\nPliva: " + plovi + 
+                "\nVozi: " + vozi + "\nLeti: " + leti + "\nBroj kotača: " + +brojKotaca;
+
+        }
+    }
+}
---
+++ KinkelaVito06_02_2019/KinkelaVito06_02_2019/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KinkelaVito06_02_2019.Klase;
+
+namespace KinkelaVito06_02_2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vozilo avion = new Vozilo();
+
+            avion.setBrojKotaca(6);
+            avion.setLeti(true);
+            avion.setPlovi(false);
+            avion.setVozi(true);
+            avion.setVrsta("Cessna");
+            avion.setOznaka("1312");
+
+            Console.WriteLine(
+                "Vrsta " + avion.getVrsta() +
+                "\nOznaka: " + avion.getOznaka() +
+                "\nBroj kotača: " + avion.getBrojKotaa() +
+                "\nPlovi: " + avion.getPlovi() +
+                "\nVozi: " + avion.getVozi() +
+                "\nLeti: " + avion.getLeti() + "\n"
+                );
+
+            Vozilo kamojon = new Vozilo(8, false, false, true, "RI1987AR", "TAM");
+            Console.WriteLine(kamojon.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
